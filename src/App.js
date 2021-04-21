952268c6df8136d6b8d813092173412e85a6d879
Keturah Howard <keturah.dev@gmail.com>
2021-04-21 12:49:32 -0700

    doc next steps to come back to later

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
     <div className="App">
       <header>
         <h1>Welcome to Catter!</h1>
-        
+        <p>The app that nobody asked for, but aren't too upset about discovering.</p>
+
+        {/* Ideas */}
+{/* pick a component library */}
+{/* pick a free cat api */}
+{/* create a component that randomly generates cat photos */}
+{/* create a component that randomly generates cat facts */}
+
       </header>
       {/* <AmplifySignOut /> */}
     </div>
