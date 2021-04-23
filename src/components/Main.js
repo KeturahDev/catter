5ceec9a4f1f958aeea238b48f32430e50e0a9ce1
Keturah Howard <keturah.dev@gmail.com>
2021-04-23 16:39:30 -0700

    brain storm main view

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+function Main (content) {
+  // const [state, setState] = useState('default');
+  // useEffect(() => {
+  //   //fetch goes here
+  // })
+
+  // let defaultComponenet;
+  // state !== 'default' ? setState(content) : null;
+
+  // 
+  return (
+    <React.Fragment>
+      <div style={{border: '2px solid green'}}>
+        This is the main view where imma put stuff.
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Main;
