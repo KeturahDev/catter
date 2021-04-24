d0e5fd1752c16692817a46e0305e261c800b3395
Keturah Howard <keturah.dev@gmail.com>
2021-04-23 17:28:46 -0700

    utilize styled components and begin fetching

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,44 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+
+
+const Button = styled.button `
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 1em 1em;
+  padding: 0.25em 1em;
+`
+const Box = styled.div `
+  diplay: flex;
+  margin: 20%;
+  padding: 5%;
+  border: 2px solid orange;
+`
+
 
 function Main (content) {
-  // const [state, setState] = useState('default');
-  // useEffect(() => {
-  //   //fetch goes here
-  // })
+ 
+  let catImage;
+  const getCatImage = async () => {
+    try {
+      let data = await fetch('https://api.thecatapi.com/v1/images/search?size=full')
+      console.log("image is: ", data.json()[0]["url"])
+    } catch (err) {
+      console.log('error: ', err)
+    }
+  }
 
-  // let defaultComponenet;
-  // state !== 'default' ? setState(content) : null;
-
-  // 
   return (
     <React.Fragment>
-      <div style={{border: '2px solid green'}}>
-        This is the main view where imma put stuff.
-      </div>
+      <Box>
+        <div>
+          What happens if you press the button?
+        </div>
+        <Button onClick={() => getCatImage()}>Cat</Button>
+      </Box>
     </React.Fragment>
   )
 }
