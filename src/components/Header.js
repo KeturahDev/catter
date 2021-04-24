9f079a6aa76ab5a85893e9572fa9fe7517bac18b
Keturah Howard <keturah.dev@gmail.com>
2021-04-24 13:21:04 -0700

    small touch ups

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
+
 
 function Header () {
     return (
       <React.Fragment>
-        <header>Catter</header>
+        <h1>Catter</h1>
         <p>The app that nobody asked for, but aren't too upset about discovering.</p>
       </React.Fragment>
     )
